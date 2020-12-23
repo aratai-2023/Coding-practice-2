21c3958f288e0b2653e233abe914d9b2296db1ec
Taishi A <arataitai@gmail.com>
2020-12-23 16:55:11 +0900

    Complete the dynamic layer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+  // bx slider
   $('.slider').bxSlider({
-    mode: 'fade'
+    mode: 'fade',
+    // auto: true,
+    speed: 200
+    // pause: 3500,
+    // autoHover: true
+  });
+
+  // 斜め背景に動きをつける
+  $('.topics').on({
+    'mouseover': function() {
+      $('.topics').addClass('tpc-active');
+    },
+    'mouseout': function() {
+      $('.topics').removeClass('tpc-active');
+    }
+  });
+  
+  $('.recruit').on({
+    'mouseover': function() {
+      $('.recruit').addClass('rct-active');
+    },
+    'mouseout': function() {
+      $('.recruit').removeClass('rct-active');
+    }
   });
 });
